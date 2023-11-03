da488e00959ff5dddbdc6235fd3a28ec1fbb1c3a
u31511 <u31511@P31511>
2023-11-03 16:06:02 +1000

    //

---
+++ WindowQuest2.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Stas_PR5
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowQuest2.xaml
+    /// </summary>
+    public partial class WindowQuest2 : Window
+    {
+        public WindowQuest2()
+        {
+            InitializeComponent();
+            Manager.MyFrame = MyFrame;
+        }
+
+        private void Case1_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MyFrame.Navigate(new Case1());
+        }
+
+        private void Case2_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MyFrame.Navigate(new Case2());
+        }
+
+        private void Case3_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MyFrame.Navigate(new Case3());
+        }
+
+        private void Case4_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MyFrame.Navigate(new Case4());
+        }
+
+        private void Case5_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MyFrame.Navigate(new Case5());
+        }
+    }
+}
